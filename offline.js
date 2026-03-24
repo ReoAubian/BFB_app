6abe1fd408a8475b2e2bbaaed717f66cb8ebf298
ReoAubian <144679935+ReoAubian@users.noreply.github.com>
2026-03-24 01:46:27 +0100

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1774313144,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ui_logo-sheet0.png",
+		"images/spr_case-sheet0.png",
+		"images/spr_case-sheet1.png",
+		"images/ui_text.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
